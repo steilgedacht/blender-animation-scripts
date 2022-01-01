ec1abaa66f716011310a782e6bd1ced68efd9dc2
Benjamin Bergmann <89748204+steilgedacht@users.noreply.github.com>
2022-01-01 17:27:38 +0100

    Update hinaus.py

--- hinaus.py
+++ hinaus.py
@@ -1,20 +1,24 @@
 import bpy
 import random
+
+#duration in [frames]
+duration = 60 
+
 name = bpy.context.active_object.name
 richtung = random.randint(1,4)
 
 frame = bpy.data.scenes['Scene'].frame_current
 bpy.ops.anim.keyframe_insert_menu(type='Location')
-bpy.context.scene.frame_set(frame + 60)
+bpy.context.scene.frame_set(frame + duration)
 
 if richtung == 1:
     bpy.data.objects[name].location = (5,0,0) #rechts
 if richtung == 2:
-    bpy.data.objects[name].location = (-5,0,0)#links
+    bpy.data.objects[name].location = (-5,0,0) #links
 if richtung == 3:
-    bpy.data.objects[name].location = (0,-5,0)#unten
+    bpy.data.objects[name].location = (0,-5,0) #unten
 if richtung == 4:
-    bpy.data.objects[name].location = (0,5,0)#oben
+    bpy.data.objects[name].location = (0,5,0) #oben
     
 bpy.ops.anim.keyframe_insert_menu(type='Location')
 bpy.context.scene.frame_set(frame)
